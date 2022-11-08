3badd7eba10e95ab6c8d1b470a4f86df7eb2835a
Constantin Nshuti <constantin.nshuti@mni.thm.de>
2022-11-08 19:16:44 +0100

    Update Hausuebung/src/hausuebung1/CodecGui.java, Hausuebung/src/hausuebung1/Wuerfel.java Deleted Hausuebung/src/hausuebung1/Klasse.java

--- Hausuebung/src/hausuebung1/Wuerfel.java
+++ Hausuebung/src/hausuebung1/Wuerfel.java
@@ -1,6 +1,55 @@
 package hausuebung1;
 
 public class Wuerfel implements Codec {
+	/**
+     * String, welcher Schluessel/Losungwort bildet
+     * wird durch den Uebergabeparameter der dazugehoerigen Setter-Methode initialisiert.
+     */
+    private String schluessel;
+
+    /**
+     * String, welcher den modifizierten Text bildet.
+     *
+     * @param schluessel String.
+     */
+
+
+    public Wuerfel(String schluessel) {
+
+        this.schluessel = schluessel;
+    }
+
+    /**
+     * Methode zum Finden Postion von jedem Buchstabe, in dem die Buchstaben eines Schluesselwort sortiert und Ihr Position finden,
+     * und in einem Array permutation speichern.
+     *
+     * @param schluessel String, welcher je nach Postion jeder Buchstabe ein Text kodiert oder dekodiert werden muss.
+     * @return String Array, der Position der Buchstaben enthält.
+     */
+
+    private int[] suchePosition(String schluessel) {
+        this.schluessel = schluessel.toUpperCase();
+
+        int[] permutation = new int[this.schluessel.length()];
+        int[] position = new int[this.schluessel.length()];
+
+        for (int i = 0; i < this.schluessel.length(); i++) {
+            int count = 1;
+
+            for (int j = 0; j < this.schluessel.length(); j++) {
+                if (this.schluessel.charAt(i) > this.schluessel.charAt(j) | (this.schluessel.charAt(i) == this.schluessel.charAt(j) && i > j)) {
+                    count++;
+                }
+            }
+            position[i] = count;
+        }
+
+        for (int i = 0; i < position.length; i++) {
+            permutation[position[i] - 1] = i;
+        }
+        return permutation;
+    }
+
 	
 public String kodiere(String klartext) {
 	return null;
