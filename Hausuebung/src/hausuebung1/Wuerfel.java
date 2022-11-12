494a69cde868bb9f0224549c5a0804c79696e510
Constantin Nshuti <constantin.nshuti@mni.thm.de>
2022-11-12 14:11:30 +0100

    wuerfel

--- Hausuebung/src/hausuebung1/Wuerfel.java
+++ Hausuebung/src/hausuebung1/Wuerfel.java
@@ -1,7 +1,21 @@
-package hausuebung1;
+package hue1;
+/**
+ * Eine Klasse die das Inteface Codec implementiert und alle darin erhaltenen Methoden bereitstellt.
+ * Klasseninvariante : Die Variablen "schluessel".
+ * A. Der Schluessel soll niemals leer sein
+ * B. Der Schluessel soll kein Sonderzeichen enthalten
+ * C. Der Schluessel muss nur  Buchstaben enthalten
+ * Die Variable "schluessel" ist auf einer Seite Verschluesselung eines klartextes bzw. eines verschluesselten Textes,
+ * und auf anderen Seite zur Entschluesselung verschluesselter Text.
+ *
+ *
+ * @author Constantin Nshuti
+ */
+
 
 public class Wuerfel implements Codec {
-	/**
+
+    /**
      * String, welcher Schluessel/Losungwort bildet
      * wird durch den Uebergabeparameter der dazugehoerigen Setter-Methode initialisiert.
      */
@@ -50,25 +64,99 @@ public class Wuerfel implements Codec {
         return permutation;
     }
 
-	
-public String kodiere(String klartext) {
-	return null;
-		
-		
-	} 
-	public String dekodiere(String geheimtext) {
-		return null;
-		
-	}
-	public String gibLosung() {
-		return null;
-		
-	} 
-	public void setzeLosung(String schluessel)throws IllegalArgumentException{
-		
-		
-	} // bei ungeeignetem Schlüssel! }
+    /**
+     * Methode zum Verschluesseln eines Klartextes.
+     * Die Position jeweiliger Buchstabe von klartext gemaeß die Postion von Buchstaben des schluesselwortes ordnen,
+     * in dem die Buchstaben mit gleichen Position zusammengesetzt sein muessen, da wird das Position des gefundenene Buchstabe
+     * mit der Laenge des schluesselwortes addiert und weiter so.
+     *
+     * @param klartext String, der Text der spaeter kodiert sein wird.
+     */
+    public String kodiere(String klartext) {
 
+        int[] permutation = suchePosition(this.schluessel);
+
+        StringBuilder geheimtext = new StringBuilder();
+
+        for (int i = 0; i < permutation.length; i++) {
+            for (int j = 0; j < klartext.length(); j++) {
+
+                if (j == permutation[i]) {
+                    while (j < klartext.length()) {
+                        geheimtext.append(klartext.charAt(j));
+                        j = j + schluessel.length();
+                    }
+                }
+
+            }
+
+        }
+
+        return geheimtext.toString();
+    }
+
+
+    /**
+     * Methode zum Entschluesseln eines Geheimtextes
+     * Die Position jeweiliger Buchstabe von geheimtext gemäß die Postion von Buchstaben des schluesselwortes ordnen,
+     * in dem die Buchstaben mit gleichen Position zusammengesetzt sein müssen, da wird der gefundenene
+     * Buchstabe in StringBuilder neu_text rein gesetzt und der nachfolge geloescht.
+     *
+     * @param geheimtext String, der den Ausgangstext zurueck nach dem Entschuesseln
+     */
+    public String dekodiere(String geheimtext) {
+
+        int[] permutation = suchePosition(this.schluessel);
+
+        StringBuilder neu_text = new StringBuilder(geheimtext);
+        int position = 0;
+
+        for (int i = 0; i < permutation.length; i++) {
+            for (int j = 0; j < geheimtext.length(); j++) {
+                if (permutation[i] == j % permutation.length) {
+                    neu_text.insert(j, geheimtext.charAt(position));
+                    neu_text.deleteCharAt(j + 1);
+                    position++;
+
+                }
+            }
+
+        }
+
+        return neu_text.toString();
+    }
+
+    /**
+     * den Schluessel liefern, welcher vorher definiert wurde.
+     * sollte diese nicht vorher definiert worden sein, so wird ein standardmaeßiger Schluessel "ABC" gesetzt.
+     *
+     * @return String schluessel.
+     */
+    public String gibLosung() {
+
+        return this.schluessel;
+    }
+
+    /**
+     * Den Schluessel aus Uebergabeparameter setzen.
+     * Die beide Schluessel ist bei Wuerfel Verschluesselung sehr wichtig und vorrausgesetzt, da der Klartext zweimal verschluesselt sein muss.
+     * Was ausgegeben werden, erklärt alles
+     *
+     * @param schluessel String.
+     */
+    public void setzeLosung(String schluessel) throws IllegalArgumentException {
+
+
+        if (schluessel.length() == 0) {
+            throw new IllegalArgumentException(" Das Schluesselwort soll niemals leer sein  ");
+        }
+        if (schluessel.matches(".*\\d.*") == true) {
+            throw new IllegalArgumentException("Das Schluesselwort muss nur  Buchstaben enthalten");
+        }
+        if (schluessel.matches(".*[!@#$%&()_+=|<>?{}\\[\\]~-].*") == true) {
+            throw new IllegalArgumentException("Das Schluesselwort soll kein Sonderzeichen enthalten");
+
+        }
+        this.schluessel = schluessel;
+    }
 }
-
-
